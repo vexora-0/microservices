2f35693d5fedd9c5fb07c57057f5e01cf8ef7b63
Bhargav <143892094+vexora-0@users.noreply.github.com>
2024-12-20 12:00:00 +0000

    added

---
+++ order_service/src/main/java/com/egov/order_service/controller/OrderController.java
@@ -0,0 +1,15 @@
+package com.egov.order_service.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/orders")
+public class OrderController {
+
+    @GetMapping
+    public String getOrders() {
+        return "Hello World from Order Service!";
+    }
+}
---
+++ user_service/src/main/java/com/egov/user_service/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.egov.user_service.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @GetMapping
+    public String getUsers() {
+        return "Hello World from User Service!";
+    }
+}
